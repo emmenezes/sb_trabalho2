db976cb8fb5be82e47c15421dac44d500c01dbaf
Emme Menezes <menezes.ecd@gmail.com>
2022-09-17 21:23:13 -0300

    feat: adiciona arquivo com funções auxiliares

--- src/auxiliar.asm
+++ src/auxiliar.asm
@@ -14,31 +14,15 @@ divi:
     idiv ebx
 
 ; pesquisar sobre variáveis locais
-input:
-    ; recebe ponteiro de onde ficará o int
-    ; cria variavel global/local para receber número em ascii
-    ; receber número
-    ; checar se é o fim
-    ; carregar o primeiro número
-    ; subtrair 0x30
-    ; somar no acumulador
-    ; voltar
-    ; se for o fim, salvar número no ponteiro
-    ; voltar
 
-    push dop1
-    push word op2
-    call sum
 s_input:
     push ebp
     mov ebp, esp
-    push eax
     sub edx, edx
     mov eax, 3
     mov ebx, 0
     mov ecx, [ebp+10]
     mov dx, [ebp+8]
     int 80h
-    pop eax
     pop ebp
     ret 10
